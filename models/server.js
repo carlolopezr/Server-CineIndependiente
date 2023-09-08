ef09180c51ed1948d64bdeee3e49cf5ec93bdc47
Carlos Lopez <carlo.lopezr@duocuc.cl>
2023-09-08 19:53:42 -0300

    Added cors 2

--- models/server.js
+++ models/server.js
@@ -24,14 +24,10 @@ class Server {
     middlewares() {
 
         // CORS
-        this.app.use(cors());
+        this.app.use(cors({
+            allowedHeaders:'Access-Control-Request-Headers'
+        }));
 
-        this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            next();
-          });
         // Lectura y parseo del body
         this.app.use(express.json());
 
