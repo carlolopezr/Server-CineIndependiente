a6a08e92159fcf18be8736d12770dc38d84a13e9
Carlos Lopez <carlo.lopezr@duocuc.cl>
2023-09-08 16:47:21 -0300

    register fixed 3

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getUser, postUser, getUsers} = require('../controllers/user');
+const { getUser, postUser, getUsers, requestEmailVerification} = require('../controllers/user');
 
 const router = Router();
 
@@ -8,7 +8,7 @@ router.get('/', getUser)
 router.post('/', postUser)
 
 router.get('/users', getUsers)
-router.post('/emailVerification')
+router.post('/emailVerification', requestEmailVerification)
 
 
 module.exports = router;
