8fdfdb003e1c17f3a732e5e0766c37c9ea55b956
Carlos Lopez <carlo.lopezr@duocuc.cl>
2023-10-15 13:40:30 -0300

    Added update-delete directors

--- controllers/director.js
+++ controllers/director.js
@@ -2,40 +2,75 @@ const { request, response } = require('express');
 const { prisma } = require('../database/config');
 
 
-const updateDirector = async (req = request, res = response) => {
-    try {
-        const { directors = [] } = req.body;
-        const updatedDirectors = [];
+// const updateDirector = async (req = request, res = response) => {
+//     try {
+//         const { directors = [] } = req.body;
+//         const updatedDirectors = [];
 
-        for (const director of directors) {
-            try {
-                const updatedDirector = await prisma.director.update({
-                    where: {
-                        director_id: director.director_id
-                    },
-                    data: {
-                        name: director.name,
-                    }
-                });
+//         for (const director of directors) {
+//             try {
+//                 const updatedDirector = await prisma.director.update({
+//                     where: {
+//                         director_id: director.director_id
+//                     },
+//                     data: {
+//                         name: director.name,
+//                     }
+//                 });
 
-                updatedDirectors.push(updatedDirector);
-            } catch (err) {
+//                 updatedDirectors.push(updatedDirector);
+//             } catch (err) {
                 
-                console.error(`Error actualizando director con ID ${director.director_id}:`, err);
-                throw err;
+//                 console.error(`Error actualizando director con ID ${director.director_id}:`, err);
+//                 throw err;
+//             }
+//         }
+//         res.status(200).json({
+//             msg: 'Directores actualizados con éxito',
+//             updatedDirectors
+//         });
+//     } catch (error) {
+//         res.status(500).json({
+//             msg: 'Hubo un error al actualizar los directores',
+//             error
+//         });
+//     }
+// };
+
+const updateDirector = async(req=request, res=response) => {
+    try {
+        const {directors=[], movie_id} = req.body
+
+        const updatedMovie = await prisma.movie.update({
+            where: {
+                movie_id:movie_id
+            },
+            data:{
+                directors:{
+                    deleteMany:{},
+                    createMany:{
+                        data:directors
+                    }
+                }
             }
-        }
-        res.status(200).json({
-            msg: 'Directores actualizados con éxito',
-            updatedDirectors
-        });
+        }).catch((err) => {
+            console.log(err);
+        })   
+
+        return res.status(200).json({
+            msg:'Directores actualizados con éxito',
+            updatedMovie
+        })
+
+
     } catch (error) {
         res.status(500).json({
-            msg: 'Hubo un error al actualizar los directores',
+            msg:'Hubo un error al actualizar los directores',
             error
-        });
+        })
     }
-};
+}
+
 
 module.exports = {
     updateDirector
